97ff0abdd1760c910f394823961ab8fedea50441
nukkua <marco.salazar.2014.com@gmail.com>
2024-09-29 19:26:03 -0400

    home page almost finished

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,6 +1,38 @@
+import { useEffect } from "react";
+import { Navbar } from "../Common/Navbar"
+import { FilterBy } from "./components/FilterBy"
+import { SearchBar } from "./components/SearchBar"
+import { GymGrid } from "./components/GymGrid";
+
 export const Home = () => {
+	const filterOptions = [
+		{ value: 'highest-rated', label: 'Mejor valorado' },
+		{ value: 'nearest', label: 'Mas cercano' },
+		{ value: 'lowest-price', label: 'Mas barato' }
+	];
+	const dropDownMenuList = [
+		{ name: 'Iniciar Sesion', url: '/login', icon: <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11 20H19C20.1046 20 21 19.1046 21 18V6C21 4.89543 20.1046 4 19 4H11M3 12H14M14 12L11 15M14 12L11 9" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg> },
+
+	];
+
+
+	const handleFilterChange = (selectedOption) => {
+		console.log('Selected Filter:', selectedOption);
+	};
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [])
 	return (
-		<div>Home</div>
+
+		<div className="flex flex-col min-h-screen bg-fourth sm:items-center">
+			<Navbar dropDownMenuList={dropDownMenuList} />
+			<SearchBar />
+			<FilterBy options={filterOptions} onSelect={handleFilterChange} />
+			<GymGrid />
+
+
+
+		</div>
 	)
 }
 
---
+++ src/Screens/Home/components/FilterBy.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+export const FilterBy = ({ options, onSelect }) => {
+	const [selectedOption, setSelectedOption] = useState('');
+
+	const handleSelect = (event) => {
+		const value = event.target.value;
+		setSelectedOption(value);
+		onSelect(value); // Pasar la selección al componente padre   
+	};
+
+	return (
+
+		<section className="px-2 py-2 flex items-center gap-3">
+			<p className='text-lg font-bold sm:text-xl xl:text-2xl'>Filtrar por</p>
+			<select
+				id="filter-dropdown"
+				value={selectedOption}
+				onChange={handleSelect}
+				aria-label="Filter by"
+				className="block ml-auto px-4 py-2 border-2 border-gray-300 rounded-md focus:outline-none focus:ring-primary focus:border-primary"
+			>
+				{options.map((option, index) => (
+					<option
+						key={index}
+						value={option.value}
+						className="text-gray-900 bg-white hover:bg-gray-100"
+					>
+						{option.label}
+					</option>
+				))}
+			</select>
+		</section>
+
+	);
+};
+
