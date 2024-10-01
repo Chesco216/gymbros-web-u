4c950fbae9509dbf75cf93dece7141d3942c5f5b
nukkua <marco.salazar.2014.com@gmail.com>
2024-10-01 17:57:40 -0400

    pancakes

--- src/Screens/Auth/services/auth.jsx
+++ src/Screens/Auth/services/auth.jsx
@@ -3,29 +3,29 @@ import { auth } from "../../../firebase/firebasse"
 
 export const signupEP = async (email, password) => {
 
-  try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-    const user = userCredential.user
-    localStorage.setItem('user', JSON.stringify(user))
-    window.location.href = '/'
+	try {
+		const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+		const user = userCredential.user
+		localStorage.setItem('user', JSON.stringify(user))
+		window.location.href = '/'
 
-    return user
-  } catch (error) {
-    alert(`Error: ${error.code}`)
-  }
+		return user
+	} catch (error) {
+		alert(`Error: ${error.code}`)
+	}
 
 }
 
 export const loginEP = async (email, password) => {
 
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password)
-    const user = userCredential.user
-    localStorage.setItem('user', JSON.stringify(user))
-    window.location.href = '/'
+	try {
+		const userCredential = await signInWithEmailAndPassword(auth, email, password)
+		const user = userCredential.user
+		localStorage.setItem('user', JSON.stringify(user))
+		window.location.href = '/'
 
-    return user
-  } catch (error) {
-    alert(`Error: ${error.code}`)
-  }
+		return user
+	} catch (error) {
+		alert(`Error: ${error.code}`)
+	}
 }
