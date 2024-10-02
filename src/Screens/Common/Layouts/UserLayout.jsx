0041ec4707a51b11d254ded2fc516f107d707901
nukkua <marco.salazar.2014.com@gmail.com>
2024-10-02 16:21:59 -0400

    a la mierda

--- src/Screens/Common/Layouts/UserLayout.jsx
+++ src/Screens/Common/Layouts/UserLayout.jsx
@@ -1,11 +1,14 @@
 import { Navbar } from "../Navbar"
+import './UserLayout.css'
 
 export const UserLayout = ({ children }) => {
 	return (
 
-		<div className="flex flex-col min-h-screen bg-fourth sm:items-center">
+		<div className="flex flex-col min-h-screen bg-fourth overflow-custom">
 			<Navbar />
-			{children}
+			<div className="flex flex-col min-h-screen justify-center sm:items-center w-full">
+				{children}
+			</div>
 		</div>
 
 	)
--- src/Screens/Home/components/GymGrid.jsx
+++ src/Screens/Home/components/GymGrid.jsx
@@ -2,8 +2,8 @@ import { GymCard } from "./GymCard"
 
 export const GymGrid = ({ gyms }) => {
 	return (
-		<section className="px-2 py-4 ">
-			<ul className="flex flex-col gap-3 2xl:gap-5">
+		<section className="px-2 py-4">
+			<ul className="flex flex-col gap-3 2xl:gap-5 xl:max-w-[1000px]">
 
 				{
 					gyms.map((g) => (
