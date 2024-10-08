49bae4fe54fc99f833d2f4f5a43fc69318fe6317
nukkua <marco.salazar.2014.com@gmail.com>
2024-10-08 07:45:43 -0400

    adding little changes again

---
+++ src/Screens/Home/components/Welcome.jsx
@@ -0,0 +1,13 @@
+export const Welcome = () => {
+	return (
+
+
+		<div className="w-full flex justify-center mt-24">
+			<section className="px-2 pt-10 pb-4 flex gap-3 w-full md:max-w-3xl lg:max-w-4xl xl:max-w-7xl items-center">
+
+				<p className='text-xl font-semibold sm:text-2xl xl:text-3xl tracking-wide'>Hola. Encuentra tu gimnasio ideal!</p>
+			</section>
+		</div>
+
+	)
+}
