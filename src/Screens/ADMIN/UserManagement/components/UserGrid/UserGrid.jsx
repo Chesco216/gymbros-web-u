904960e32bee28d5971bc54b2ca1239843a215a5
nukkua <marco.salazar.2014.com@gmail.com>
2024-10-23 23:31:03 -0400

    pan con queso

--- src/Screens/ADMIN/UserManagement/components/UserGrid/UserGrid.jsx
+++ src/Screens/ADMIN/UserManagement/components/UserGrid/UserGrid.jsx
@@ -1,29 +1,36 @@
 import { UserCard } from '../UserCard/UserCard'
-import styles from './UserGrid.module.css'
 
-export const UserGrid = ({ users, setIsOpen, setMod }) => {
+export const UserGrid = ({ users, setIsOpen, setMod, setUpdateUser }) => {
 
 	return (
-		<div className={styles.container}>
-			<span className={styles.headers}>
-				<label className={`${styles.labels} `}>Nombre</label>
-				<label className={`${styles.labels} `}>CI</label>
-				<label className={`${styles.labels} `}>Estado</label>
-				<label className={`${styles.labels} `}>Expira</label>
-				<label className={`${styles.labels} `}>Plan</label>
-			</span>
-			{
-				users.map(user => <UserCard
-					key={user.name}
-					name={user.name}
-					ci={user.ci}
-					isActive={user.isActive}
-					expires={user.expires_at}
-					plan={user.plan}
-					setIsOpen={setIsOpen}
-					setMod={setMod}
-				/>)
-			}
+		<div className="overflow-x-auto mt-5">
+			<table className="w-full text-left rtl:text-right text-lg">
+				<thead className="text-gray-500 font-light ">
+					<tr>
+						<th scope="col" className="px-6 py-3">Nombre</th>
+						<th scope="col" className="px-6 py-3">CI</th>
+						<th scope="col" className="px-6 py-3">Estado</th>
+						<th scope="col" className="px-6 py-3">Expira</th>
+						<th scope="col" className="px-6 py-3">Plan</th>
+					</tr>
+				</thead>
+				<tbody>
+
+					{
+						users.map(user => <UserCard
+							key={user.name}
+							name={user.name}
+							ci={user.ci}
+							isActive={user.isActive}
+							expires={user.expires_at}
+							plan={user.plan}
+							setIsOpen={setIsOpen}
+							setMod={setMod}
+							setUpdateUser={setUpdateUser}
+						/>)
+					}
+				</tbody>
+			</table>
 		</div>
 	)
 }
