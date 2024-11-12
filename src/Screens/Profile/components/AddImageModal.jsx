dc0b2c7cf36757f38ad327037ad3da67b64b2d46
Chesco216 <franchescofv198@gmail.com>
2024-11-12 10:57:57 -0400

    just in case my carger dies

--- src/Screens/Profile/components/AddImageModal.jsx
+++ src/Screens/Profile/components/AddImageModal.jsx
@@ -1,23 +1,36 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import { useUser } from '../../../store/useUser'
 
 export const AddImageModal = ({setIsOpen}) => {
 
+  const user = useUser(state => state.user)
+
   const [img, setImg] = useState()
+
+  const handleInputImage = (e) => {
+    console.log(e.target.files[0])
+    const urlRef = e.target.files[0]
+    setImg(URL.createObjectURL(urlRef))
+  }
+
   const getImageRef = async() => {
     const imgRef = ref((store, `user/${img.name}`))
     await uploadBytes(imgRef)
-    const downloadRef = ref(store, `gs://gymbros-f0bab.appspot.com/user/${img.name}`)
+    const downloadRef = ref(store, `gs://gymbros-f0bab.appspot.com/user/${user.uid}.jpg`)
     const imgUrl = getDownloadURL(downloadRef)
     return imgUrl
   }
 
+  //NOTE: mi skill issue con tailwind pipipi, lo centras porfis
   return (
-    <form>
-      <p>Selecciona una imagen para tu perfil</p>
-      <input type='file' accept='.jpg, .jpeg, .png' onChange={(e) => setImg(e.target.files[0])}/>
-      <button>Subir foto de perfil</button>
-      <button onClick={() => setIsOpen(false)}>Cerrar</button>
-    </form>
+      <form className={`absolute bg-white flex flex-col`}>
+        <p>Selecciona una imagen para tu perfil</p>
+      {
+        (img) && <img src={img} className='w-[80px] h-[80px]'/>
+      }
+        <input type='file' accept='.jpg, .jpeg, .png' onChange={(e) => handleInputImage(e)}/>
+        <button>Subir foto de perfil</button>
+        <button onClick={() => setIsOpen(false)}>Cerrar</button>
+      </form>
   )
 }
-
