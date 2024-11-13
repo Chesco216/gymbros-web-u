b6a9bf9c19d0fe312107ed7c9ddc24be9126debf
nukkua <marco.salazar.2014.com@gmail.com>
2024-11-13 10:03:09 -0400

    pan

--- src/Screens/ADMIN/UserManagement/UserManagement.jsx
+++ src/Screens/ADMIN/UserManagement/UserManagement.jsx
@@ -30,8 +30,12 @@ export const UserManagement = () => {
 	}, [])
 
 	const handleRefresh = () => {
-		navigate("/admin/users");
+		getUsers().then((us) => {
+			setUsers(us);
+			setUsersFiltered(us);
+		})
 	}
+
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -50,6 +54,8 @@ export const UserManagement = () => {
 			<div className={`w-full flex justify-center py-10 fade-in`}>
 				<section className="flex flex-col 2xl:w-[1300px] bg-white rounded-xl p-16 shadow-md">
 					<h1 className="text-4xl font-bold mb-5 slide-in">Gestion de Clientes</h1>
+
+
 					<div className="flex w-full gap-4 mb-5">
 						<form onSubmit={handleSubmit} className="flex gap-4" action="">
 							<input
@@ -68,6 +74,10 @@ export const UserManagement = () => {
 							</button>
 
 						</form>
+
+		<button className="border-2 border-black py-1 px-2 rounded-xl " onClick={handleRefresh}>
+							<svg className="w-7 h-7 hover:animate-spin" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18.6091 5.89092L15.5 9H21.5V3L18.6091 5.89092ZM18.6091 5.89092C16.965 4.1131 14.6125 3 12 3C7.36745 3 3.55237 6.50005 3.05493 11M5.39092 18.1091L2.5 21V15H8.5L5.39092 18.1091ZM5.39092 18.1091C7.03504 19.8869 9.38753 21 12 21C16.6326 21 20.4476 17.5 20.9451 13" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+						</button>
 						<button
 							onClick={() => {
 								setIsOpen(true);
